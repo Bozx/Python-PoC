fa91973c1800a3dbd9e11e83e4d1d07f2a87729c
Bozx <38514671+Bozx@users.noreply.github.com>
2024-02-13 02:50:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Bozkurt97 - Intigriti, subdomain takeover!"
+    return "<h1>Bozkurt97 - Intigriti, subdomain takeover!<img src=x onerror=confirm(9)>"
